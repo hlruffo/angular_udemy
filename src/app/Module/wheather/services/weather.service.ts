3628f97b88a8a73411d2da6da6ff9a1147ab2c9e
hlruffo <hlruffo@gmail.com>
2023-11-02 16:01:05 -0300

    fix : importação observable no serviço weather

--- src/app/Module/wheather/services/weather.service.ts
+++ src/app/Module/wheather/services/weather.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +10,7 @@ export class WeatherService {
   constructor(
     private httpClient: HttpClient
   ) { }
-  getWeatherData(cityName:string): Observable<any>{
+  getWeatherDatas(cityName:string): Observable<any>{
     return this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`, {})
   }
 }
