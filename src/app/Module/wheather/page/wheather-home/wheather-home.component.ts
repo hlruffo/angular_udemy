bf8198350c1d02ded5ea69a0efc0c1809f8f2eab
hlruffo <hlruffo@gmail.com>
2023-11-02 16:01:39 -0300

    consumo da api - criação do componente para consumo

--- src/app/Module/wheather/page/wheather-home/wheather-home.component.ts
+++ src/app/Module/wheather/page/wheather-home/wheather-home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/Models/intefaces/WeatherDatas';
 
 @Component({
   selector: 'app-wheather-home',
   templateUrl: './wheather-home.component.html',
   styleUrls: []
 })
-export class WheatherHomeComponent {
+export class WheatherHomeComponent implements OnInit {
+  initialCityName = 'Rio de Janeiro';
+  weatherDatas!: WeatherDatas;
+
+  constructor(private weatherService: WeatherService){}
+
+  ngOnInit(): void{
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+
+  getWeatherDatas(cityName: string): void{
+    this.weatherService.getWeatherDatas(cityName).subscribe({
+        next:(response: any) =>{
+          response && (this.weatherDatas =response)
+          console.log(this.weatherDatas)
+        },
+        error:(error: any)=>console.log(error),
+      });
+  }
 
 }
