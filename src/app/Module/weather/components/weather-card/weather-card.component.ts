e990d5c784d2a7a9250c65d2b57ed9471c06a443
hlruffo <hlruffo@gmail.com>
2023-11-08 10:13:06 -0300

    feat: implementação do card filho

--- src/app/Module/weather/components/weather-card/weather-card.component.ts
+++ src/app/Module/weather/components/weather-card/weather-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { WeatherDatas } from './../../../../Models/intefaces/WeatherDatas';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: []
 })
-export class WeatherCardComponent {
+export class WeatherCardComponent implements OnInit {
+  
+  
+  @Input() WeatherDatasInput!: WeatherDatas;
 
+
+  ngOnInit(): void {
+    console.log("Dados recebidos do componente pai:",
+    this.WeatherDatasInput);
+  }
 }
